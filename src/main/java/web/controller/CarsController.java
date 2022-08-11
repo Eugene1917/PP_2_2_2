8518d812427d7f7e091b198d68914be76e541f2b
Eugene1917 <Bloody.30@mail.ru>
2022-08-11 15:56:51 +0400

    FixUpByEugene

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -1,11 +1,13 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
+import web.service.CarService;
 import web.service.CarServiceImpl;
 import java.util.List;
 
@@ -13,10 +15,16 @@ import java.util.List;
 @RequestMapping("/cars")
 public class CarsController {
 
+    private CarService carService;
+
+    @Autowired
+    public CarsController(CarService carService) {
+        this.carService = carService;
+    }
+
     @GetMapping()
     public String printCars(@RequestParam(defaultValue="5") Integer count, ModelMap model) {
-        CarServiceImpl carsServiceImpl = new CarServiceImpl();
-        List<Car> qtyCars = carsServiceImpl.carList(count);
+        List<Car> qtyCars = carService.carList(count);
         model.addAttribute("cars", qtyCars);
         return "cars";
     }
--- src/main/java/web/model/Car.java
+++ src/main/java/web/model/Car.java
@@ -3,16 +3,11 @@ import lombok.*;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Car {
     private String model;
     private String series;
     private int year;
-
-    public Car(String model, String series, int year) {
-        this.model = model;
-        this.series = series;
-        this.year = year;
-    }
 
     @Override
     public String toString() {
